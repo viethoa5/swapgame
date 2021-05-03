c6cf2f2e4f0b69e62dfdf93d88c8701af31bfae2
viethoa5 <65301131+viethoa5@users.noreply.github.com>
2021-05-03 21:31:34 +0700

    Add files via upload

--- game.cpp
+++ game.cpp
@@ -1,73 +1,118 @@
-#include <iostream>
-#include <string>
-#include <fstream>
+#include<iostream>
+#include<string>
 #include <vector>
-#include <cstdlib>
 #include <ctime>
-// int choseword : chose a random word from list
-// string shuffle : swap letter from word chosen
-// void print : print word chosen
-// string checkwordsimple : check the letter from word
-// bool checkfullword : check full word
-// string swapletter : swap letter from  swapped word to find find true word
+#include <cstdlib>
+#include<stdio.h>
+#include<conio.h>
+#include<time.h>
+#include<Windows.h>
+#include <fstream>
 using namespace std;
-int choseword(int n) {
-   int randomnum = rand() % n;
-   return randomnum;
+const int rows = 5;
+const int cols = 2;
+
+
+void clrscr(void)
+{
+CONSOLE_SCREEN_BUFFER_INFO csbiInfo;
+HANDLE hConsoleOut;
+COORD Home = {0,0};
+DWORD dummy;
+
+hConsoleOut = GetStdHandle(STD_OUTPUT_HANDLE);
+GetConsoleScreenBufferInfo(hConsoleOut,&csbiInfo);
+
+FillConsoleOutputCharacter(hConsoleOut,' ',csbiInfo.dwSize.X * csbiInfo.dwSize.Y,Home,&dummy);
+csbiInfo.dwCursorPosition.X = 0;
+csbiInfo.dwCursorPosition.Y = 0;
+SetConsoleCursorPosition(hConsoleOut,csbiInfo.dwCursorPosition);
 }
-string shuffle(string wordguessed) {
+void gotoXY(short x,short y)
+{
+HANDLE hConsoleOutput;
+COORD Cursor_an_Pos = { x,y};
+hConsoleOutput = GetStdHandle(STD_OUTPUT_HANDLE);
+SetConsoleCursorPosition(hConsoleOutput , Cursor_an_Pos);
+}
+
+enum TrangThai {hidden,shown};
+enum gameplay {wrong,win,lost,right};
+
+struct board{
+int value;
+TrangThai tt;
+char word;
+};
+
+struct Game{
+board boards[1][5];
+int rows;
+int cols;
+};
+string shuffle(string wordguessed,Game & game) {
  for (int i = wordguessed.length() -1; i >0; i--) {
-    swap(wordguessed[i],wordguessed[i-1]);
+    int j = rand() % (i+1);
+    swap(wordguessed[i],wordguessed[j]);
+    swap(game.boards[0][i].value,game.boards[0][j].value);
  }
  return wordguessed;
 }
-void print(string s) {
-    for (int i = 0; i < s.length(); i++) {
-        cout << s[i];
+
+void creatboard(string s1,Game & game){
+
+for(int i = 0; i < rows; i++) {
+    game.boards[1][i].word = s1[i];
+    game.boards[1][i].value = i;
+    game.boards[0][i].value = i;
+    game.boards[1][i].tt=hidden;
+}
+     s1 = shuffle(s1,game);
+     for(int i = 0; i < rows; i++) {
+    game.boards[0][i].word = s1[i];
+    game.boards[0][i].tt= shown;
+}
+}
+void showboard(Game &game){
+    clrscr();
+for (int i = 0;i < cols; i++) {
+    for(int j = 0; j < rows; j++) {
+        if(game.boards[i][j].tt == shown) {
+        cout << game.boards[i][j].word;
+    }
+    else cout << "_" ;
     }
     cout << endl;
-}
-string checkwordsimple(string wordchange,string wordguess, int a, int b) {
-    if (a > wordchange.length() || b > wordchange.length()) {
-        return "wrong";
-    }
-    else {
-     if (wordchange[a] == wordguess[b] || wordchange[b] == wordguess[a] ) {
-        return "true";
-     }
-     else return "wrong";
-    }
-
-}
-bool checkfullword(string s1, string s2) {
-for (int i = 0; i < s1.length(); i++) {
-    if (s1[i] != s2[i]) {
-       return false;
-       break;
-    }
-}
-return true;
+  }
 }
 
-string swapletter(string wordchange,string wordguess) {
-    int num1,num2;
-    cin >> num1 >> num2;
-    string check = checkwordsimple(wordchange,wordguess,num1,num2);
-    if (check == "wrong") {
-        print(check);
-        swapletter(wordchange,wordguess);
-    }
-    else {
-        swap(wordchange[num1],wordchange[num2]);
-        print(wordchange);
-       if(checkfullword(wordchange,wordguess) == 0) swapletter(wordchange,wordguess);
-    }
-   return "victory";
+bool checkword(Game & game,int &num1,int &num2) {
+   if (game.boards[0][num1-1].value == game.boards[1][num2-1].value) {
+    return true;
+   }
+ return false;
 }
-
-
-
-int main () {
+bool checkfullword(Game & game) {
+    for(int j = 0; j < rows; j++) {
+        if(game.boards[1][j].tt == hidden) {
+            return false;
+            break;
+        }
+    }
+    return true;
+}
+void swapword(Game &game,int &num1, int &num2) {
+    if (checkword(game,num1,num2) == true) {
+        game.boards[1][num2-1].tt = shown;
+        game.boards[0][num1-1].word = '*';
+    }
+}
+int choseword(int n) {
+   srand((unsigned) time(NULL));
+   int res =  rand() % n;
+   return res;
+}
+int main() {
 ifstream fileInput("C:/Users/ADMIN/Desktop/file.txt");
 if (fileInput.fail())
 	{
@@ -84,12 +129,23 @@ if (fileInput.fail())
      fileInput >> word[i];
 	}
 	string wordguessed = word[choseword(n)];
-	string wordchange = shuffle(wordguessed);
-	while(checkfullword(wordchange,wordguessed) == 1) {
-	   wordchange = shuffle(wordchange);
-	}
-   print(wordchange);
-   wordchange = swapletter(wordchange,wordguessed);
-   print(wordchange);
-    return 0;
+  board boards;
+  Game game;
+  creatboard(wordguessed,game);
+  while(checkfullword(game) == false) {
+    showboard(game);
+    int num1,num2;
+    cin >> num1 >> num2;
+    swapword(game,num1,num2);
+    }
+   for(int i = 0 ; i < cols; i++) {
+    cout << game.boards[0][i].value << " ";
+   }
+
+  showboard(game);
+  return 0;
+
 }
+
+
+
